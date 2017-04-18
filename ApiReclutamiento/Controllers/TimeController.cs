28c66e414de84fbd4b20e878d86672bd9dba58aa
Linkunder <linkunder@hotmail.com>
2017-04-18 09:29:03 -0300

    Api

---
+++ ApiReclutamiento/Controllers/TimeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web.Http;
+
+namespace ApiReclutamiento.Controllers
+{
+    public class TimeController : ApiController
+    {
+        public IHttpActionResult Get(string value)
+        {
+            string hora;
+            string horaFormateada;
+            
+            //Se comprueba lo que ha llegado por get, el numero 8 es considerando el formato HH:MM:SS
+            if (value != null && Regex.IsMatch(value, "^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$"))
+            {
+
+                hora = value;
+                DateTime utc = new DateTime();
+                //Se setean los valores de HH:MM:SS de manera exacta.
+                utc = DateTime.ParseExact(hora, "HH:mm:ss",System.Globalization.CultureInfo.InvariantCulture);
+
+                //Se transforma el DateTime utc a UTC.
+                horaFormateada = utc.ToUniversalTime().ToString();
+                
+           
+                
+
+                return Ok(new
+                {
+                    code = "00",
+                    description = "OK",
+                    data = horaFormateada
+
+
+                });
+            }
+            else
+            {
+                return BadRequest("400 - bad request.");
+                
+            }
+        
+        }
+        public IHttpActionResult GetError()
+        {
+            return InternalServerError();
+        }
+
+
+    }
+}
---
+++ ApiReclutamiento/Controllers/WordController.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Web.Http;
+
+namespace ApiReclutamiento.Controllers
+{
+    public class WordController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Post(JObject jsonData)
+        {
+            //Se obtiene el Json
+            dynamic json = jsonData;
+            //Se accede al data que contiene la palabra que se debe cambiar a mayus
+            try
+            {
+                if (json.data != null)
+                {
+                    string palabra = json.data;
+
+                    if (Regex.IsMatch(palabra, "[a-z]{4}"))
+                    {
+                        return Ok(
+                            new
+                            {
+                               code = 00 , descripton = "OK", data = palabra.ToUpper()
+                            }
+                                );
+
+                    }
+                }
+            }catch(Exception e)
+            {
+                
+                return InternalServerError(e);
+            }
+            return BadRequest("400-Bad Request.");
+
+    
+
+        }
+
+
+    }
+}
